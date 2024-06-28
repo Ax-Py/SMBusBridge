be55945f29befd225b23f44a4696dff8f144b315
Ax-Py <138998779+Ax-Py@users.noreply.github.com>
2024-06-28 18:13:51 -0400

    Update bridge_commands_328P.h

--- include/bridge_commands_328P.h
+++ include/bridge_commands_328P.h
@@ -16,14 +16,14 @@ uint8_t rising_edge_pulse();												// For devices that trigger on the risin
 
 uint8_t falling_edge_pulse();												// For devices that trigger on the falling edge of a pulse (e.g. 34401A DMM external trigger, oscilloscope external trigger)
 
-uint8_t GPIO_HIGH();														// Force GPIO HIGH (can be used for enable/disable signal on external device)
+uint8_t GPIO_HIGH();														    // Force GPIO HIGH (can be used for enable/disable signal on external device)
 
-uint8_t GPIO_LOW();															// Force GPIO LOW (can be used for enable/disable signal on external device)
+uint8_t GPIO_LOW();															    // Force GPIO LOW (can be used for enable/disable signal on external device)
 
-void I2C_broadcast(uint16_t *data_array);								// I2C interpreter that does not check if any slave devices are connected. This can be used to check that the master is operating properly on its own.
+void I2C_broadcast(uint16_t *data_array);						// I2C interpreter that does not check if any slave devices are connected. This can be used to check that the master is operating properly on its own.
 
-uint8_t I2C_arbitration(uint16_t *data_array);								// I2C interpreter that checks the state machine of the I2C peripheral. This can only be used when there is at least one slave device on the bus.
+uint8_t I2C_arbitration(uint16_t *data_array);			// I2C interpreter that checks the state machine of the I2C peripheral. This can only be used when there is at least one slave device on the bus.
 
-uint8_t UART_receive_array(uint8_t data_byte);		// Receive data from PC serial terminal and parse it according to its value
+uint8_t UART_receive_array(uint8_t data_byte);		  // Receive data from PC serial terminal and parse it according to its value
 
 #endif /* BRIDGE_COMMANDS_328P_H_ */
